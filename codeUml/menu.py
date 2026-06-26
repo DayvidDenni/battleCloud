bab0bda150e22f9c8edfe1da788dc87e5089ce4c
Dayvid Denni <dayvid_prados@hotmail.com>
2026-06-25 22:07:58 -0300

    Adding class generated from StarUml

---
+++ codeUml/menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Class2 import Class2
+
+
+class Menu(Class2):
+    def __init__(self):
+        self.window = None
+
+    def run(self, ):
+        pass
