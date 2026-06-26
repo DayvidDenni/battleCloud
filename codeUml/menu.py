bcd6d109356d80944790db8c9f0deb6e5504950e
Dayvid Denni <dayvid_prados@hotmail.com>
2026-06-25 22:44:12 -0300

    Main loop classes menu and game connecting

--- codeUml/menu.py
+++ codeUml/menu.py
@@ -5,8 +5,7 @@ from Class2 import Class2
 
 
 class Menu(Class2):
-    def __init__(self):
-        self.window = None
-
+    def __init__(self,window):
+        self.window = window
     def run(self, ):
         pass
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from codeUml.game import Game
 
-print("Setup Start")
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-print("Setup End")
-
-print("loop Start")
-while True:
-    # check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # Close window
-            quit()  # end pygame
+game = Game()
+game.run()
